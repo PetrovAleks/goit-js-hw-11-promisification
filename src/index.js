09498792eb988eec9d76ba4583cbd13a51efe525
Petrov Aleksandr <rpm10b@gmail.com>
2020-07-05 17:18:03 +0300

    сделал 3 таскеа дз 11-promise

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import './styles.css';
+import './script/task-1';
+import './script/task-2';
+import './script/task-3';
---
+++ src/script/task-3.js
@@ -0,0 +1,32 @@
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const makeTransaction = transaction => {
+  const delay = randomIntegerFromInterval(200, 500);
+  const canProcess = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (canProcess) {
+        return resolve({ id: transaction.id, time: delay });
+      }
+      return reject(transaction.id);
+    }, delay);
+  });
+};
+
+const logError = id => {
+  return console.warn(
+    `Error processing transaction ${id}. Please try again later.`,
+  );
+};
+const logSuccess = ({ id, time }) => {
+  return console.log(`Transaction ${id} processed in ${time}ms`);
+};
+makeTransaction({ id: 70, amount: 150 }).then(logSuccess).catch(logError);
+
+makeTransaction({ id: 71, amount: 230 }).then(logSuccess).catch(logError);
+
+makeTransaction({ id: 72, amount: 75 }).then(logSuccess).catch(logError);
+
+makeTransaction({ id: 73, amount: 100 }).then(logSuccess).catch(logError);
